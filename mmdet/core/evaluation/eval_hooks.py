gsd : 0.15 & 0.40 gsd normalization bug 수정 sliding window gpu 메모리 부족한 문제 완화

mmdet/core/evaluation/eval_hooks.py
@@ -65,18 +65,28 @@ class DistEvalHook(Hook):
         if not force_run and not self.every_n_epochs(runner, self.interval):
             return
         runner.model.eval()
+
         results = [None for _ in range(len(self.dataset))]
         prog_bar = mmcv.ProgressBar(len(self.dataset))
         for idx in range(runner.rank, len(self.dataset), runner.world_size):
             data = self.dataset[idx]
-            img = self.dataset.get_img(idx)
-            data_gpu = scatter(collate([data], samples_per_gpu=1), [torch.cuda.current_device()])[0]
+
+            # print(len(data['img']))
+            # if len(data['img']) > 0:     # TODO
+            data = collate([data], samples_per_gpu=1)
+            data['img_meta'] = [x.data[0] for x in data['img_meta']]
+            lazy = True
+            # else:
+            #     data = scatter(collate([data], samples_per_gpu=1), [torch.cuda.current_device()])[0]
+            #     lazy = False
 
             # compute output
             with torch.no_grad():
-                result = runner.model(return_loss=False, rescale=True, **data_gpu)
+                result = runner.model(return_loss=False, rescale=True, lazy=lazy, **data)
 
+            # log result images
             from mmdet.apis import show_result
+            img = self.dataset.get_img(idx)
             # img = mmcv.imresize(img, data)
             show_result(img, result, dataset='dota', score_thr=0.01, out_file=os.path.join(self.debug_root, 'result%05d.jpg' % idx))
             results[idx] = result

mmdet/datasets/dota.py
@@ -31,6 +31,9 @@ logger.addHandler(ch)
 
 _dota_root = '/data/public/rw/team-autolearn/aerial/DOTA/v1.5_hbb_190402/'
 _size = 1024
+_target_gsd = (0.15, 0.4)
+_gsd_aug = 0.01
+_overlap = (0.5, 0.5)
 
 
 class DotaPreprocess:
@@ -43,7 +46,7 @@ class DotaDataset(Dataset, DotaPreprocess):
                  img_norm_cfg=None, img_scale=(1333, 800), resize_keep_ratio=True, flip_ratio=0.,
                  size_divisor=32, test_mode=False, **kwargs):
         super(DotaDataset, self).__init__(target_gsd)
-        print(kwargs.keys())
+        print('unresolved arguments', kwargs.keys())
         self.CLASSES = wordname_16
 
         self.settype = settype
@@ -58,10 +61,10 @@ class DotaDataset(Dataset, DotaPreprocess):
         self.dota = DOTA(root)
 
         x = self.dota.imglist
+        y = []
         if 'cv' in settype:
             # stratified split
             cv = 0  # TODO
-            y = []
             cnt_obj = 0
             for id in x:
                 anns = self.dota.ImgToAnns[id]
@@ -87,17 +90,38 @@ class DotaDataset(Dataset, DotaPreprocess):
         logger.info('settype=%s filtered, size=%d' % (self.settype, len(self.index)))
         assert len(self.index) > 0
 
+        # oversampling
+        if 'train' in settype:
+            ratio = [1, 2, 4, 4, 2,
+                     1, 1, 1, 2, 4,
+                     2, 4, 1, 2, 2,
+                     8]
+            over_index = []
+            for id, lb in zip(self.index, y):
+                max_ratio = max([r if onehot_lb == 1 else 1 for r, onehot_lb in zip(ratio, lb)])
+                over_index.extend([id] * max_ratio)
+            random.shuffle(over_index)
+            self.index = over_index
+            logger.info('oversampled, size=%d' % len(self.index))
+
         # transforms
         self.multiscale_mode = 'range'
         self.img_scales = img_scale if isinstance(img_scale, list) else [img_scale]
         if not img_norm_cfg:
             img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+        self.img_norm_cfg = img_norm_cfg
+        if test_mode:
+            img_norm_cfg = dict(mean=[0, 0, 0], std=[1, 1, 1], to_rgb=False)
         self.img_transform = ImageTransform(size_divisor=size_divisor, **img_norm_cfg)
         self.bbox_transform = BboxTransform()
         self.numpy2tensor = Numpy2Tensor()
 
         # gsd normalization
-        self.gsd_aug = GSDNormalizedCrop(crop_size=(_size, _size), random_crop=(not self.test_mode))  # TODO : parameters
+        self.gsd_aug = GSDNormalizedCrop(
+            crop_size=(_size, _size),
+            random_crop=(not self.test_mode),
+            target_gsd=_target_gsd
+        )  # TODO : parameters
 
         # if use extra augmentation
         if extra_aug is not None:
@@ -138,13 +162,14 @@ class DotaDataset(Dataset, DotaPreprocess):
         gsd = self.dota.gsd(imgid)
         gt_bboxes, gt_labels = self.get_ann_info(index)
 
-        # gsd normalization & random crop
-        if self.gsd_aug:
-            img, gt_bboxes, gt_labels, gsd_scale = self.gsd_aug(img, gt_bboxes, gt_labels, gsd)
-        else:
-            gsd_scale = 1.
-
         if not self.test_mode:
+            # gsd normalization & random crop
+            if self.gsd_aug:
+                selected_cls = random.choice(list(set(gt_labels)))
+                img, gt_bboxes, gt_labels, gsd_scale = self.gsd_aug(img, gt_bboxes, gt_labels, gsd, -1, selected_cls)
+            else:
+                gsd_scale = 1.
+
             # extra augmentation
             if self.extra_aug is not None:
                 img, gt_bboxes, gt_labels = self.extra_aug(img, gt_bboxes, gt_labels)
@@ -165,8 +190,7 @@ class DotaDataset(Dataset, DotaPreprocess):
                 ori_shape=ori_shape,
                 img_shape=img_shape,
                 pad_shape=pad_shape,
-                scale_factor=scale_factor,
-                gsd_scale_factor=gsd_scale,
+                scale_factor=scale_factor * gsd_scale,
                 flip=flip)
 
             data = dict(
@@ -181,26 +205,30 @@ class DotaDataset(Dataset, DotaPreprocess):
         else:
             imgs = []
             img_metas = []
-            # for img_scale in self.img_scales:
-            # sliding window
-            windows = sw.generate(img, sw.DimOrder.HeightWidthChannel, _size, 0.5)
-            for window in windows:
-                _img_w = img[window.indices()]
+            for gsd_idx in range(len(_target_gsd)):
+                img_g, gt_bboxes_g, gt_labels_g, gsd_scale_g = self.gsd_aug(img, gt_bboxes, gt_labels, gsd, gsd_idx, -1)
 
-                for flip_h in [False, True]:
-                    _img, _img_shape, _pad_shape, _scale_factor = self.img_transform(_img_w, self.img_scales[0], flip_h, keep_ratio=self.resize_keep_ratio)
-                    _gt_bboxes = self.bbox_transform(gt_bboxes, _img_shape, _scale_factor, flip_h)
+                # 각 window마다 매번 normalize하는게 느려서 따로 처리하도록 함
+                img_g = mmcv.imnormalize(img_g, self.img_norm_cfg['mean'], self.img_norm_cfg['std'], True)
 
-                    imgs.append(to_tensor(_img))
-                    img_metas.append(DC(dict(
-                        ori_shape=ori_shape,
-                        img_shape=(_img.shape[1], _img.shape[2], 3),
-                        pad_shape=(_img.shape[1], _img.shape[2], 3),
-                        translation=(window.x, window.y),
-                        scale_factor=_scale_factor,
-                        gsd_scale_factor=gsd_scale,
-                        flip=flip_h
-                    ), cpu_only=True))
+                # sliding window
+                windows = sw.generate(img_g, sw.DimOrder.HeightWidthChannel, _size, _overlap[gsd_idx])
+                for window in windows:
+                    _img_w = img_g[window.indices()]
+
+                    for flip_h in [False]:
+                        _img, _img_shape, _pad_shape, _scale_factor = self.img_transform(_img_w, self.img_scales[0], flip_h, keep_ratio=self.resize_keep_ratio)
+                        # _gt_bboxes = self.bbox_transform(gt_bboxes_g, _img_shape, _scale_factor, flip_h)
+
+                        imgs.append(to_tensor(_img))
+                        img_metas.append(DC(dict(
+                            ori_shape=ori_shape,
+                            img_shape=(_img.shape[1], _img.shape[2], 3),
+                            pad_shape=(_img.shape[1], _img.shape[2], 3),
+                            translation=(window.x, window.y),
+                            scale_factor=_scale_factor * gsd_scale_g,
+                            flip=flip_h
+                        ), cpu_only=True))
 
             data = dict(img=imgs, img_meta=img_metas)
             return data
@@ -228,12 +256,20 @@ class GSDNormalizedCrop(object):
         self.target_gsd = target_gsd
         self.random_crop = random_crop
 
-    def __call__(self, img, boxes, labels, gsd):
+    def __call__(self, img, boxes, labels, gsd, target_idx=-1, selected_cls=-1):
         # resize (gsd normalization)
         target_gsd = self.target_gsd
         if isinstance(self.target_gsd, (list, tuple)):
-            target_gsd = random.choice(self.target_gsd)
-        scale = target_gsd / gsd
+            if target_idx >= 0:
+                target_gsd = target_gsd[target_idx]
+            else:
+                target_gsd = random.choice(target_gsd)
+
+        if target_gsd < 0:
+            # augment
+            target_gsd += random.uniform(-_gsd_aug, _gsd_aug)
+
+        scale = gsd / target_gsd
         img = mmcv.imrescale(img, scale)
         boxes = (boxes.astype(np.float64) * scale + 0.5).astype(np.int64)
 
@@ -252,7 +288,7 @@ class GSDNormalizedCrop(object):
         while not_cropped:
             min_iou = random.choice(self.min_iou)
 
-            for i in range(50):
+            for i in range(100):
                 left = random.uniform(0, w - new_w)
                 top = random.uniform(0, h - new_h)
 
@@ -260,6 +296,10 @@ class GSDNormalizedCrop(object):
                 overlaps = bbox_overlaps(patch.reshape(-1, 4), boxes.reshape(-1, 4)).reshape(-1)
                 if overlaps.min() < min_iou:
                     continue
+                if selected_cls >= 0:
+                    overlap_idx = overlaps > min_iou
+                    if not (labels[overlap_idx] == selected_cls).any():
+                        continue
 
                 # center of boxes should inside the crop img
                 center = (boxes[:, :2] + boxes[:, 2:]) / 2

mmdet/models/detectors/base.py
@@ -53,7 +53,7 @@ class BaseDetector(nn.Module):
         l1 = []
         l2 = []
         for img, img_meta in zip(imgs, img_metas):
-            det_bboxes, det_scores, _, _ = self._simple_test(img, img_meta, proposals, True, return_lb=False)
+            det_bboxes, det_scores, _, _ = self._simple_test(img, img_meta, proposals, False, return_lb=False)
             if det_bboxes.shape[0] == 0:
                 continue
             assert det_bboxes.shape[1] == 4
@@ -65,8 +65,6 @@ class BaseDetector(nn.Module):
             tran = img_meta[0].get('translation', (0, 0))
             det_bboxes = bbox_mapping_back(det_bboxes, img_shape, scale_factor, flip, tran=tran)
             assert det_bboxes.shape[1] == 4
-            gsd_scale_factor = img_meta[0]['gsd_scale_factor']
-            det_bboxes[:, :4] = det_bboxes[:, :4] / gsd_scale_factor
 
             l1.append(det_bboxes)
             l2.append(det_scores)
@@ -74,18 +72,18 @@ class BaseDetector(nn.Module):
         merged_scores = torch.cat(l2, dim=0)
         assert merged_scores.max() <= 1.
 
-        det_bboxes, det_labels = multiclass_nms(
+        merged_bboxes, merged_labels = multiclass_nms(
             merged_bboxes, merged_scores, self.test_cfg.rcnn.score_thr,
             self.test_cfg.rcnn.nms, self.test_cfg.rcnn.max_per_img
         )
 
         # threshold
-        inds = det_bboxes[:, -1] > self.test_cfg.rcnn.score_thr
-        det_bboxes = det_bboxes[inds, :]
-        det_labels = det_labels[inds]
+        inds = merged_bboxes[:, -1] > self.test_cfg.rcnn.score_thr
+        merged_bboxes = merged_bboxes[inds, :]
+        det_labels = merged_labels[inds]
 
         # nms cross classes
-        det_bboxes, inds = nms(det_bboxes, 0.7)  # TODO
+        det_bboxes, inds = nms(merged_bboxes, 0.7)  # TODO
         det_labels = det_labels[inds]
 
         assert det_bboxes[:, 4].max() <= 1.
@@ -93,8 +91,8 @@ class BaseDetector(nn.Module):
         # filter out small boxes
         valid_idx = det_bboxes[:, 0] >= 0
         valid_idx = valid_idx * (det_bboxes[:, 1] >= 0)
-        valid_idx = valid_idx * (det_bboxes[:, 0] < img_metas[0][0]['ori_shape'][1] * img_metas[0][0]['gsd_scale_factor'])
-        valid_idx = valid_idx * (det_bboxes[:, 1] < img_metas[0][0]['ori_shape'][0] * img_metas[0][0]['gsd_scale_factor'])
+        valid_idx = valid_idx * (det_bboxes[:, 0] < img_metas[0][0]['ori_shape'][1])
+        valid_idx = valid_idx * (det_bboxes[:, 1] < img_metas[0][0]['ori_shape'][0])
         valid_idx = valid_idx * ((det_bboxes[:, 2] - det_bboxes[:, 0]) > 2)
         valid_idx = valid_idx * ((det_bboxes[:, 3] - det_bboxes[:, 1]) > 2)
         _det_bboxes = det_bboxes[valid_idx, :]
@@ -116,18 +114,15 @@ class BaseDetector(nn.Module):
 
     def forward_test(self, imgs, img_metas, **kwargs):
         for var, name in [(imgs, 'imgs'), (img_metas, 'img_metas')]:
-            if not isinstance(var, list):
-                raise TypeError('{} must be a list, but got {}'.format(
-                    name, type(var)))
+            if not isinstance(var, (list, tuple)):
+                raise TypeError('{} must be a list, but got {}'.format(name, type(var)))
 
         num_augs = len(imgs)
         if num_augs != len(img_metas):
-            raise ValueError(
-                'num of augmentations ({}) != num of image meta ({})'.format(
-                    len(imgs), len(img_metas)))
+            raise ValueError('num of augmentations ({}) != num of image meta ({})'.format(len(imgs), len(img_metas)))
         # TODO: remove the restriction of imgs_per_gpu == 1 when prepared
         imgs_per_gpu = imgs[0].size(0)
-        assert imgs_per_gpu == 1
+        assert imgs_per_gpu == 1, imgs_per_gpu
 
         # if num_augs == 1:
         #     return self.simple_test(imgs[0], img_metas[0], **kwargs)

mmdet/models/detectors/cascade_rcnn.py
@@ -190,6 +190,11 @@ class CascadeRCNN(BaseDetector, RPNTestMixin, BBoxTestMixin):
         return losses
 
     def _simple_test(self, img, img_meta, proposals=None, rescale=False, return_lb=True):
+        if not img.is_cuda:
+            img = img.cuda(torch.cuda.current_device())
+            created = True
+        else:
+            created = False
         x = self.extract_feat(img)
         proposal_list = self.simple_test_rpn(x, img_meta, self.test_cfg.rpn) if proposals is None else proposals
 
@@ -221,8 +226,7 @@ class CascadeRCNN(BaseDetector, RPNTestMixin, BBoxTestMixin):
                     scale_factor,
                     rescale=rescale,
                     cfg=rcnn_test_cfg)
-                bbox_result = bbox2result(det_bboxes, det_labels,
-                                          bbox_head.num_classes)
+                bbox_result = bbox2result(det_bboxes, det_labels, bbox_head.num_classes)
                 ms_bbox_result['stage{}'.format(i)] = bbox_result
 
                 if self.with_mask:
@@ -247,8 +251,7 @@ class CascadeRCNN(BaseDetector, RPNTestMixin, BBoxTestMixin):
 
             if i < self.num_stages - 1:
                 bbox_label = cls_score.argmax(dim=1)
-                rois = bbox_head.regress_by_class(rois, bbox_label, bbox_pred,
-                                                  img_meta[0])
+                rois = bbox_head.regress_by_class(rois, bbox_label, bbox_pred, img_meta[0])
 
         cls_score = sum(ms_scores) / self.num_stages
         det_bboxes, det_labels = self.bbox_head[-1].get_det_bboxes(
@@ -259,6 +262,8 @@ class CascadeRCNN(BaseDetector, RPNTestMixin, BBoxTestMixin):
             scale_factor,
             rescale=rescale,
             cfg=rcnn_test_cfg)
+        if created:
+            del img
         return det_bboxes, det_labels, ms_bbox_result, ms_segm_result
 
     def simple_test(self, img, img_meta, proposals=None, rescale=False):
@@ -317,5 +322,4 @@ class CascadeRCNN(BaseDetector, RPNTestMixin, BBoxTestMixin):
         else:
             if isinstance(result, dict):
                 result = result['ensemble']
-        super(CascadeRCNN, self).show_result(data, result, img_norm_cfg,
-                                             **kwargs)
+        super(CascadeRCNN, self).show_result(data, result, img_norm_cfg, **kwargs)